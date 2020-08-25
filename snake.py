6466c3dfde5197e0fb57b2ab7497056717d22d28
Mattia <59755795+matt185@users.noreply.github.com>
2020-08-25 12:29:42 +0900

    add movemt command and body adding new part

--- snake.py
+++ snake.py
@@ -2,15 +2,16 @@
 
 import turtle
 import random
+import time
 
 # declare variable 
-
+delay= 0.05
 # setup the screen 
 
 game_area = turtle.Screen()
 game_area.title("Snake Game")
 game_area.bgcolor("lightgreen")
-game_area.setup(width=500, height=500)
+game_area.setup(width=600, height=600)
 game_area.tracer(0) # for control of the animation 
 
 # set the snake head
@@ -20,7 +21,7 @@ head.speed(0)
 head.shape("circle")
 head.color("black")
 head.penup() 
-head.goto(random.randint(-245, 245),random.randint(-245, 245))
+head.goto(random.randint(-290, 290),random.randint(-290, 290))
 head.direction = "stop" # at the beginning the speed is 0
 
 body_part = []
@@ -34,13 +35,13 @@ token.color("purple")
 token.penup()
 
 if head.distance(token) < 10:   
-    x = random.randint(-245, 245)
-    y = random.randint(-245, 245)
-    token.goto(x,y)
+    token.goto(random.randint(-290, 290),random.randint(-290, 290))
 else:
-    token.goto(random.randint(-245, 245),random.randint(-245, 245))
+    token.goto(random.randint(-290, 290),random.randint(-290, 290))
 
-# set the control and movement  
+
+# set the control and movement function 
+
 def up_direction():
     head.direction = "up"
 
@@ -54,26 +55,57 @@ def right_direction():
     head.direction = "right"
 
 def move():
-    if head.direction == "up":
-        head.sety(head.ycor() + 10)
-    
+    if head.direction == "up": 
+        head.sety(head.ycor() + 20)
+  
     if head.direction == "down":
-        head.sety(head.ycor() - 10)
+        head.sety(head.ycor() - 20)
     
     if head.direction == "right":
-        head.setx(head.xcor() + 10)
+        head.setx(head.xcor() + 20)
         
     if head.direction == "left":
-        head.setyx(head.xcor()+10)
+        head.setx(head.xcor()-20)
 
-    
+# set keyboard keys
 
+game_area.listen()
+game_area.onkeypress(up_direction, "Up")
+game_area.onkeypress(down_direction, "Down")
+game_area.onkeypress(left_direction, "Left")
+game_area.onkeypress(right_direction, "Right")
 
-
+ 
 # set the game
 while True:
-   game_area.update()
+    game_area.update()
 
+    #when token hit
+    if head.distance(token) < 20:
+        #move it to different place
+        token.goto(random.randint(-290, 290), random.randint(-290, 290))
+
+        #add a part to the body
+        new_part = turtle.Turtle()
+        new_part.speed(0)
+        new_part.shape("square")
+        new_part.color("violet")
+        new_part.penup()
+        body_part.append(new_part)
+    #rearrange the body    
+    for index in range(len(body_part) - 1, 0, -1):
+        x = body_part[index - 1].xcor()
+        y = body_part[index - 1].ycor()
+        body_part[index].goto(x, y)
+    
+    #place the head
+
+    if len(body_part) > 0:
+        body_part[0].goto (head.xcor(),head.ycor())
+     
+        
+    move()
+    time.sleep(delay)
 game_area.mainloop()
 
 
